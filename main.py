3ca51d061bf53357d1608b2bc74087ff24c4d569
ali <aaskarnia@gmail.com>
2026-09-04 23:18:58 +0330

    Initialize Rock Paper Scissors game structure

---
+++ main.py
@@ -0,0 +1,13 @@
+#import and global variables
+
+
+# Create a function to get user input
+
+
+# compare and determine which one is the winner
+
+
+# Create a main function as the runner
+
+
+# make an iteration for doing the game as much as we need
